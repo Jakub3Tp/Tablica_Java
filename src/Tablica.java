5faf5802b3330050a565ae2f139b336cd2237776
kubix <jakub.gola2@teb.edu.pl>
2024-10-14 19:33:32 +0200

    Commit 3

--- src/Tablica.java
+++ src/Tablica.java
@@ -19,16 +19,45 @@ public class Tablica {
     }
 
     public int max() {
-        return Arrays.stream(tablica).max().getAsInt();
+        int max = tablica[0];
+        for (int i = 1; i < n; i++) {
+            if (tablica[i] > max) {
+                max = tablica[i];
+            }
+        }
+        return max;
     }
 
     public int min() {
-        return Arrays.stream(tablica).min().getAsInt();
+        int min = tablica[0];
+        for (int i = 1; i < n; i++) {
+            if (tablica[i] < min) {
+                min = tablica[i];
+            }
+        }
+        return min;
     }
 
     public int max2() {
-        int max = max();
-        return Arrays.stream(tablica).filter(x -> x != max).max().orElse(max);
+        int max1 = tablica[0];
+        int max2 = tablica[1];
+
+        if (max2 > max1) {
+            int temp = max1;
+            max1 = max2;
+            max2 = temp;
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (tablica[i] > max1) {
+                max2 = max1;
+                max1 = tablica[i];
+            } else if (tablica[i] > max2 && tablica[i] != max1) {
+                max2 = tablica[i];
+            }
+        }
+
+        return max2;
     }
 
     public int znajdz(int a) {
